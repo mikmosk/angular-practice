4848bd598ce797268ee60fa727d8d27426267c07
mikmosk <mikmoskv@gmail.com>
2021-10-04 00:01:30 +0300

    add ngrx tutorial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,16 +18,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, waitForAsync(() => {
+  it(`should have as title 'Home'`, waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', waitForAsync(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(app.title).toEqual('home');
   }));
 });
